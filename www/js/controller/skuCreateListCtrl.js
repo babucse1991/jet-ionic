1da6b37fd574568eb7e168fb8ac0ea0705f86d46
Babu Selvam <babucse1991@gmail.com>
2016-05-14 14:44:42 +0530

    put not working

--- www/js/controller/skuCreateListCtrl.js
+++ www/js/controller/skuCreateListCtrl.js
@@ -6,16 +6,17 @@ angular.module('skuCreateListCtrl', []) .controller('skuCreateListCtrl', functio
 	
 	$scope.productLists = [];*/
 	$scope.productList = {};
-	
+	$scope.productList.standard_product_codes=[]
 	$scope.skuCreateList= function () {
 		//var skuProducts=skuProducts;
 		//console.log(">>>>>>>>>>$scope.skuProducts.length : "+ $scope.productLists.length);
 
 		skuCreateListService.post($scope.productList).success(function (response) {
-			
+			$scope.productList.standard_product_codes.sss=productList.response;
 			//$scope.bearerToken= response;
 		//	console.log(">>>>>>1st time login : " + JSON.stringify($scope.bearerToken));
 			$scope.productLists.push($scope.productList);
+			$scope.productList.standard_product_codes.push($scope.productList.standard_product_codes);
 			//localStorageService.set(skuProducts, $scope.productLists);
 			//localStorageService.set(bearerTokenData, $scope.bearerToken);
 			
