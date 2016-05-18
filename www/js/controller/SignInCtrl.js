2073c065b5af860bce51dbcdbb52b018257684ff
babucse1991 <babucse1991@gmail.com>
2016-05-18 23:22:27 +0530

    Login page changes

--- www/js/controller/SignInCtrl.js
+++ www/js/controller/SignInCtrl.js
@@ -1,39 +1,30 @@
 angular.module('SignInCtrl', []) .controller('SignInCtrl', function($scope, $state,localStorageService, tokenService, $location, $ionicLoading ) {
 	  
-	var userData = 'user';
+	var userData = 'userApiKey';
 	var bearerTokenData = 'bearerToken';
 
-	$scope.users = [];
-	$scope.user = {
-			"user": "6F11B196321D4C79FB503AD99AC82F30982CBFBE",
-			"pass": "Kcg6nJqIOF3TojQk9sENX0QnBeI0OGHMeaF7D0P7LMDV",
-			"merchantId"	:	"b7f808daefae4f2e9d0d5303a6029df9"
-	};
+	$scope.user = {};
 
 	$scope.createLogIn = function () {
-		
-		$scope.getLogIn();
-		console.log(">>>>>>>>>>$scope.users.length : "+ $scope.users.length);
 
 		tokenService.post($scope.user).success(function (response) {
 			$ionicLoading.hide();
 			$scope.bearerToken= response;
-			console.log(">>>>>>1st time login : " + JSON.stringify($scope.bearerToken));
-			$scope.users.push($scope.user);
-			localStorageService.set(userData, $scope.users);
+			console.log("login token : " + JSON.stringify($scope.bearerToken));
+			localStorageService.set(userData, $scope.user);
 			localStorageService.set(bearerTokenData, $scope.bearerToken);
 			$location.path("/tab/dash");
-		}) ;       
-
-	}
+		}) ; 
+		
+	};
 	
-	$scope.getLogIn = function () {
-		console.log(">>>userData>>>>>"+ JSON.stringify(userData));
+	$scope.getExistingUser = function () {
         if (localStorageService.get(userData)) {
-            $scope.users = localStorageService.get(userData);
-        } else {
-            $scope.users = [];
-        }
+            $scope.user = localStorageService.get(userData);
+            $scope.createLogIn();
+        } 
 	}
-	$scope.getLogIn();
+	$scope.getExistingUser();
+	
+	
 });
